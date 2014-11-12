c602280b626836dab4ac6fc365c67d343d0d6f54
AngelMunoz <angel.dtmunoz@gmail.com>
2014-11-11 20:10:51 -0700

    Created AppTest Class to begin testing the DatabaseConnection class

---
+++ Ahorcado/src/com/test/AppTest.java
@@ -0,0 +1,23 @@
+//8:01:33 PM
+package com.test;
+
+import com.datos.DatabaseConnection;
+
+/**
+ * @author Angel Daniel Munoz Gonzalez
+ * @email scythes.lol@gmail.com
+ * @school Universidad Tecnologica de Ciudad Juarez
+ * @date Nov 11, 2014
+ * 
+ */
+public class AppTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DatabaseConnection db = new DatabaseConnection();
+	}
+
+}
