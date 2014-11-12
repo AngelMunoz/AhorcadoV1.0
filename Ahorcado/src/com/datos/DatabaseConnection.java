69eff6ea8178cdb4a5c975b24acbfd589ff63c80
AngelMunoz <angel.dtmunoz@gmail.com>
2014-11-11 20:10:22 -0700

    Created DatabaseConnection class however there is still need for the methods to extract the words from the Database

---
+++ Ahorcado/src/com/datos/DatabaseConnection.java
@@ -0,0 +1,82 @@
+//7:18:50 PM
+package com.datos;
+
+/**
+ * @author Angel Daniel Munoz Gonzalez
+ * @email scythes.lol@gmail.com
+ * @school Universidad Tecnologica de Ciudad Juarez
+ * @date Nov 11, 2014
+ * 
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class DatabaseConnection {
+	private Connection conn = null;
+	private static final String SQLDRIVER = "org.postgresql.Driver";
+	private static final String URLCONN = "jdbc:postgresql://localhost:5432/BancoPalabras";
+	private static final String USR = "Tunaxor";
+	private static final String PW = "Admin123";
+	
+	public DatabaseConnection()
+	{
+		if(checkConnection())
+		{
+			try 
+			{
+				this.conn = DriverManager.getConnection(URLCONN, USR,PW);
+			}
+			catch (SQLException e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Failed to Connect", JOptionPane.ERROR_MESSAGE);
+			}
+		}	
+	}
+	
+	
+	
+	
+	public boolean checkConnection()
+	{
+		try
+		{
+			Class.forName(SQLDRIVER);
+			conn = DriverManager.getConnection(URLCONN, USR,PW);
+			conn.close();
+			return true;
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Failed to Connect", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+	}//ends check connection
+	
+	public void close()
+	{
+		try 
+		{
+			this.conn.close();
+		} 
+		catch (SQLException e)
+		{
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Failed to Close", JOptionPane.ERROR_MESSAGE);
+		}
+		finally
+		{
+			try 
+			{
+				this.finalize();
+			}
+			catch (Throwable e)
+			{
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Failed to Close", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}//ends close method
+	
+}//ends class
+
